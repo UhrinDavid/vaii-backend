2f297aa3c1cc170f961296fa47c27090c5d39994
Dávid Uhrin <51699691+UhrinDavid@users.noreply.github.com>
2021-01-15 22:53:26 +0100

    urls modification

--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accomodationApp import views
+from rest_framework.routes import DefaultRouter
+
+router = DefaultRouter()
+router.register('rooms',views.HotelRoom)
+router.register('reviews',views.Review)
+router.register('reservations',views.Reservation)
+router.register('guests',views.Guest)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(router.urls)),
 ]
