2155f2299d2c48edd41e640b1dfc6e65f0c70700
Dávid Uhrin <51699691+UhrinDavid@users.noreply.github.com>
2021-01-15 22:31:44 +0100

    serializers

--- accomodationApp/models.py
+++ accomodationApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator 
-from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 # Create your models here.
---
+++ accomodationApp/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from accomodationApp.models import Guest,HotelRoom,Reservation,Review
+
+class HotelRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HotelRoom
+        fields='__all__'
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields='__all__'
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields='__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields='__all__'
